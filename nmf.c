6395e5956c5538d94e7d199ee7dcde85ea076e8f
fabincarmo <fabincarmo@gmail.com>
2014-10-27 09:08:22 -0200

    v0.1

--- nmf.c
+++ nmf.c
@@ -1,19 +1,24 @@
 #include <Python.h>
 #include <cblas.h>
 
+#define SUMESC(A,b,C,n,m) for(i=0; i<n;i++){for(j=0; j<m;j++){ C[j+i*m] = A[j+i*m] + b;}}
+#define SUMM(A,B,C,n,m) for(i=0; i<n;i++){for(j=0; j<m;j++){ C[j+i*m] = A[j+i*m] + B[j+i*m];}}
 #define MULT(A,B,C,n,m) for(i=0;i<n;i++){for(j=0;j<m;j++){C[i*m+j] = A[i*m+j] * B[i*m+j];}}
 #define DIV(A,B,C,n,m) for(i=0;i<n;i++){for(j=0;j<m;j++){C[i*m+j] = A[i*m+j] / B[i*m+j];}}
 #define MULTM(A,B,C,n,r,m) cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, m, r, 1.0, A, r, B, m, 0.0, C, m);
 #define MULTMtnt(A,B,C,n,r,m) cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, n, m, r, 1.0, A, n, B, m, 0.0, C, m);
 #define MULTMntt(A,B,C,n,r,m) cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, n, m, r, 1.0, A, r, B, r, 0.0, C, m);
 #define PRINTM(M,n,m) for(i=0;i<n;i++){for(j=0;j<m;j++){printf("%.5f ",M[j+i*m]);}printf("\n");}
+#define EPS 1E-20
+#define VEPS(M,n,m) for(i=0;i<n;i++){for(j=0;j<m;j++){if(M[j+i*m]<EPS){M[j+i*m] = EPS;}}}
 
-static PyObject* double_nmf(PyObject *self, PyObject *args);
 
-static PyMethodDef nmfMethods[] = {
-    {"nmf",
-        double_nmf,
-        METH_VARARGS, ""},
+static PyObject* double_nmf_euc(PyObject *self, PyObject *args);
+static PyObject* double_nmf_euc_sparse(PyObject *self, PyObject *args);
+
+static PyMethodDef nmf_eucMethods[] = {
+    {"nmf_euc", double_nmf_euc, METH_VARARGS, ""},
+	{"nmf_euc_sparse", double_nmf_euc_sparse, METH_VARARGS, ""},
     {NULL, NULL, 0, NULL}
 };
 
@@ -71,7 +76,20 @@ static double * NormaColuna(double * M, const int n, const int m){
 	return Mn;
 }
 
-static PyObject* double_nmf(PyObject *self, PyObject *args)
+static double err(double *A, double *B, double *C, int n, int r, int m){
+	int i, j;
+	double e = 0.;
+	double *R = malloc(sizeof(double) * n*m);
+	MULTM(A,B,R,n,r,m);
+	for(i=0; i<n; i++){
+		for(j=0; j<m; j++){
+			e = e + (C[j+i*m] - R[j+i*m]) * (C[j+i*m] - R[j+i*m]);
+		}
+	}
+	return e;
+}
+
+static PyObject* double_nmf_euc(PyObject *self, PyObject *args)
 {
 	int it, n, r, m, i ,j;
 	const int itmax;
@@ -98,6 +116,7 @@ static PyObject* double_nmf(PyObject *self, PyObject *args)
 		aux2 = malloc(sizeof(double) * r*m);
 		MULTMtnt(Wn, V, aux1, r,n,m);
 		MULTMtnt(Wn, R, aux2, r,n,m);
+		VEPS(aux2,r,m);
 		DIV(aux1, aux2, aux1, r,m);
 		MULT(H, aux1, H, r,m);
 		free(aux1);
@@ -108,8 +127,8 @@ static PyObject* double_nmf(PyObject *self, PyObject *args)
 		MULTM(Wn, H, R, n,r,m);
 		MULTMntt(V, H, aux1, n,m,r);
 		MULTMntt(R, H, aux2, n,m,r);
+		VEPS(aux2,n,r);
 		DIV(aux1, aux2, aux1, n,r);
-
 		MULT(W, aux1, W, n,r);
 		free(aux1);
 		free(aux2);
@@ -117,6 +136,73 @@ static PyObject* double_nmf(PyObject *self, PyObject *args)
 	}
 
 	W = NormaColuna(W,n,r);
+	printf("Erro: %g\n ",err(W,H,V,n,r,m));
+	t2 = v2list(W,n,r);
+	t3 = v2list(H,r,m);
+
+    return Py_BuildValue("OO", t2, t3);
+}
+
+static PyObject* double_nmf_euc_sparse(PyObject *self, PyObject *args)
+{
+	int it, n, r, m, i ,j;
+	const int itmax;
+	double *V, *W, *H, *R, *aux1, *aux2, *aux3, *Wn, *Ones;
+	double lambda;
+	PyObject *t1, *t2, *t3;
+
+    PyArg_ParseTuple(args, "OOOdi", &t1, &t2, &t3, &lambda, &itmax);
+
+	n = PySequence_Size(t1);
+	m = PySequence_Size(PySequence_GetItem(t1,0));
+	r = PySequence_Size(t3);
+
+	V = list2v(t1);
+	W = list2v(t2);
+	H = list2v(t3);
+
+	R = malloc(sizeof(double) * n*m);
+	Wn = malloc(sizeof(double) * n*r);
+	Ones = malloc(sizeof(double) * n*n);
+	for(i=0; i<n;i++){for(j=0; j<n;j++){ Ones[j+i*n] = 1.0;}}
+
+	for(it=0; it<itmax; it++){
+		Wn = NormaColuna(W,n,r);
+		MULTM(Wn, H, R, n, r, m);
+		aux1 = malloc(sizeof(double) * r*m);
+		aux2 = malloc(sizeof(double) * r*m);
+		MULTMtnt(Wn, V, aux1, r,n,m);
+		MULTMtnt(Wn, R, aux2, r,n,m);
+		SUMESC(aux2,lambda,aux2,r,m);
+		VEPS(aux2,r,m);
+		DIV(aux1, aux2, aux1, r,m);
+		MULT(H, aux1, H, r,m);
+		free(aux1);
+		free(aux2);
+
+		aux1 = malloc(sizeof(double) * n*r);
+		aux2 = malloc(sizeof(double) * n*r);
+		aux3 = malloc(sizeof(double) * n*r);
+		MULTM(Wn, H, R, n,r,m);
+		MULTMntt(R, H, aux1, n,m,r);
+		MULT(aux1,Wn,aux1,n,r);
+		MULTM(Ones,aux1,aux1,n,n,r);
+		MULTMntt(V,H,aux2,n,m,r);
+		SUMM(aux1,aux2,aux1,n,r);
+		MULTM(Ones,aux2,aux2,n,n,r);
+		MULT(aux2,Wn,aux2,n,r);
+		MULT(Wn,aux2,aux2,n,r);
+		MULTMntt(R, H, aux3, n,m,r);
+		SUMM(aux2,aux3,aux2,n,r);
+		VEPS(aux2,n,r);
+		DIV(aux1, aux2, aux1, n,r);
+		MULT(Wn, aux1, W, n,r);
+		free(aux1);
+		free(aux2);
+		free(aux3);
+	}
+	W = NormaColuna(W,n,r);
+	printf("Erro: %g\n ",err(W,H,V,n,r,m));
 	t2 = v2list(W,n,r);
 	t3 = v2list(H,r,m);
 
@@ -127,7 +213,7 @@ PyMODINIT_FUNC initnmf(void)
 {
     PyObject *m;
 
-    m = Py_InitModule("nmf", nmfMethods);
+    m = Py_InitModule("nmf", nmf_eucMethods);
     if (m == NULL) {
         return;
     }
--- run.py
+++ run.py
@@ -4,15 +4,28 @@ import numpy as np
 import nmf
 np.set_printoptions(precision=5, suppress=True)
 
-itmax = 1500
+itmax = 500
 
 V = np.array([[1.,1.,1.,1.,1.],[0.,1.,0.,1.,0.],[0.,1.,0.,1.,0.]])
 W = np.random.rand(3,2)
 H = np.random.rand(2,5)
 
-W2,H2 = nmf.nmf(V,W,H,itmax)
+W2,H2 = nmf.nmf_euc(V,W,H,itmax)
 
 print "V = "
 print V
 print "W . H ~"
 print np.dot(W2,H2)
+print np.array(W2)
+print np.array(H2)
+
+W2,H2 = nmf.nmf_euc_sparse(V,W,H,0.2,itmax)
+
+
+print "V = "
+print V
+print "W . H ~"
+print np.dot(W2,H2)
+print np.array(W2)
+print np.array(H2)
+
