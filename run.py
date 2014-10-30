cc7fe55ab9eced1001ad938dfc09383ee030e4b8
fabincarmo <fabincarmo@gmail.com>
2014-10-30 14:52:34 -0200

    v0.1

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import sys
 import numpy as np
 import nmf
 np.set_printoptions(precision=5, suppress=True)
@@ -12,19 +13,19 @@ H = np.random.rand(2,5)
 
 W2,H2 = nmf.nmf_euc(V,W,H,itmax)
 
-print "V = "
-print V
-print "W . H ~"
-print np.dot(W2,H2)
-print np.array(W2)
-print np.array(H2)
+print("V = ")
+print(V)
+print("W . H ~")
+print(np.dot(W2,H2))
+print(np.array(W2))
+print(np.array(H2))
 
 W2,H2 = nmf.nmf_euc_sparse(V,W,H,0.2,itmax)
 
-print "V = "
-print V
-print "W . H ~"
-print np.dot(W2,H2)
-print np.array(W2)
-print np.array(H2)
+print("V = ")
+print(V)
+print("W . H ~")
+print(np.dot(W2,H2))
+print(np.array(W2))
+print(np.array(H2))
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 module1 = Extension('nmf', sources=['pynmf.c'],
                     include_dirs=['/usr/include'],
-                    libraries=['cblas','atlas']
+                    libraries=['cblas']
                     )
 setup(name = 'nmf',
         version='1.0',
